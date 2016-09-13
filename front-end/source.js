c7bf011f091b23387a29635beaa78eb82b24afe7
James Anderson <james.r.anderson@utah.edu>
2016-09-13 13:58:48 -0600

    Support for uploading files with StructureID's or queries

--- front-end/source.js
+++ front-end/source.js
@@ -1,6 +1,74 @@
-var BASE_URL = "http://websvc1.connectomes.utah.edu/";
+var BASE_URL = "http://webdev.connectomes.utah.edu/";
 var INTRO = "http://";
 var DOMAIN = ".connectomes.utah.edu/";
+var POST_XHR = null;
+
+
+function buildIDListString(id_string) {
+    var tokens = id_string.split(/\s+/g);
+
+    formatted_output_array = [];
+    for (var i = 0; i < tokens.length; i++) {
+        formatted_token = encodeURIComponent(tokens[i]);
+        formatted_output_array.push(formatted_token);
+    }
+     
+    return formatted_output_array.join();
+}
+
+function getReportType()
+{
+    var reportTypeSelect = document.getElementById("reportTypeSelect");
+    var reportType = reportTypeSelect.value;
+    return reportType;
+}
+
+function getVolume()
+{
+    var volumeSelect = document.getElementById("volumeSelect");
+    var selectedVolume = volumeSelect.options[volumeSelect.selectedIndex].value;
+    return selectedVolume;
+}
+
+
+function getFormatType() {
+    var formatSelect = document.getElementById("formatSelect");
+    var formatType = formatSelect.options[formatSelect.selectedIndex].value;
+    return formatType;
+}
+
+function update_message(msg)
+{
+    var msgElem = document.getElementById("messages");
+    msgElem.value = msg + "\n" + msgElem.value;
+}
+
+function getQuery()
+{
+    var volumeSelect = document.getElementById("volumeSelect");
+    var selectedVolume = volumeSelect.options[volumeSelect.selectedIndex].value;
+
+    var query = "";
+    if (selectedVolume.indexOf("_") != -1) {
+        // special case alternate subdomain
+        var divIndex = selectedVolume.indexOf("_");
+
+        var subdomain = selectedVolume.substring(0, divIndex);
+        console.log(subdomain);
+
+        selectedVolume = selectedVolume.substring(divIndex + 1);
+        console.log(selectedVolume);
+
+        query = INTRO + subdomain + DOMAIN + selectedVolume + "/export/" + getReportType();
+    }
+
+    else {
+        query = BASE_URL + selectedVolume + "/export/" + getReportType();
+    }
+
+    return query + "/" + getFormatType();
+}
+
 
 function onReportTypeChanged() {
   var reportTypeSelect = document.getElementById("reportTypeSelect");
@@ -32,7 +100,7 @@ function onReportTypeChanged() {
   var dotOption = document.getElementById("fileFormat");
   if (reportType == "morphology") {
     // You can't choose this for Morphology export
-    dotOption.style.display = "none";
+    //dotOption.style.display = "none";
     var formatSelect = document.getElementById("formatSelect");
   } else {
     // You can for everything else!
@@ -46,6 +114,9 @@ function onNetworkCellIDsChanged() {
 
   var idString = networkCellIdBox.value;
 
+  if( idString)
+      idString = encodeURIComponent(idString);
+
   console.log(idString);
 
   if (idString == "") {
@@ -56,45 +127,22 @@ function onNetworkCellIDsChanged() {
   }
 }
 
+
 function onSubmitClicked() {
-  var reportTypeInput = document.getElementById("reportTypeSelect");
-  var reportType = reportTypeInput.options[reportTypeInput.selectedIndex].value;
+  var reportType = getReportType();
+  var selectedVolume = getVolume();
 
-  var volumeSelect = document.getElementById("volumeSelect");
-  var selectedVolume = volumeSelect.options[volumeSelect.selectedIndex].value;
-
-  var query = "";
-  if (selectedVolume.indexOf("_") != -1) {
-    // special case alternate subdomain
-    var divIndex = selectedVolume.indexOf("_");
-
-    var subdomain = selectedVolume.substring(0, divIndex);
-    console.log(subdomain);
-
-    selectedVolume = selectedVolume.substring(divIndex + 1);
-    console.log(selectedVolume);
-
-    query = INTRO + subdomain + DOMAIN + selectedVolume + "/export/" + reportType;
-  }
-
-  else {
-    query = BASE_URL + selectedVolume + "/export/" + reportType;
-  }
+  var query = getQuery();
 
   // TODO select the volume
 
   if (reportType == "network") {
-    var formatSelect = document.getElementById("formatSelect");
-    var formatType = formatSelect.options[formatSelect.selectedIndex].value;
-
     var cellIDs = document.getElementById("networkCellId").value;
     var hops = document.getElementById("networkHops").value;
 
-    while (cellIDs.indexOf(" ") != -1) {
-      cellIDs = cellIDs.replace(" ", "");
-    }
+    if (cellIDs)
+        cellIDs = buildIDListString(cellIDs);
 
-    query += "/" + formatType;
 
     if (cellIDs.length > 0) {
       query += "?id=" + cellIDs;
@@ -104,24 +152,75 @@ function onSubmitClicked() {
   }
 
   else if (reportType == "motif") {
-    var formatSelect = document.getElementById("formatSelect");
-    var formatType = formatSelect.options[formatSelect.selectedIndex].value;
-
     query += "s/";
-    query += formatType;
+    query += getFormatType();
   }
 
   else if (reportType == "morphology") {
-    var formatType = "tlp";
     var cellIDs = document.getElementById("morphoCellId").value;
-    while (cellIDs.indexOf(" ") != -1) {
-      cellIDs = cellIDs.replace(" ", "");
-    }
+    if (cellIDs)
+        cellIDs = buildIDListString(cellIDs);
 
-    query += "/" + formatType;
     query += "?id=" + cellIDs;
   }
 
   console.log(query);
   window.open(query);
+}
+
+function onPostStateChange() {
+    var DONE = this.DONE || 4;
+    var HEADERS_RECEIVED = this.HEADERS_RECEIVED || 2;
+    if (this.readyState == DONE && (this.status == 201 || this.status == 200)) {
+        update_message("Generated " + this.responseURL);
+        window.location = this.responseURL;
+        //this.abort(); //Do not download the body from the redirect
+    }
+}
+
+function uploadFile(endpoint, input_file_elem_id)
+{ 
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData();
+    var nif = document.getElementById(input_file_elem_id);
+    if (!nif) {
+        console.log("uploadFile: No element named " + input_file_elem_id);
+        return;
+    }
+
+    var files = nif.files;
+    if (!nif.files) {
+        console.log("uploadFile: No files property on element named " + input_file_elem_id);
+        return;
+    }
+
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+
+        // Check the file type.
+        if (!file.type.match('text/*')) {
+            continue;
+        }
+
+        // Add the file to the request.
+        formData.append('ID_file', file, file.name);
+    }
+    
+    xhr.onreadystatechange = onPostStateChange;
+    xhr.open("POST", endpoint, true);
+    xhr.send(formData);
+
+    update_message("Submitted File"); 
+}
+
+function onSubmitNetworkFileClicked() {
+    query = getQuery();
+
+    uploadFile(query, "network_ids_file");
+}
+
+function onSubmitMorphoFileClicked() {
+
+    query = getQuery();
+    uploadFile(query, "morpho_ids_file");
 }
